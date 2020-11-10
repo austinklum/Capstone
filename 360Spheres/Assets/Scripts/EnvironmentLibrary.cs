5a382a120ea8a7f68fbd1bae6fc3976d93851bdc
Austin Klum <klumaustin@gmail.com>
2020-11-10 00:16:12 -0600

    360 Photos Slideshow

---
+++ 360Spheres/Assets/Scripts/EnvironmentLibrary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+using Valve.VR.Extras;
+using System.Diagnostics;
+
+public class EnvironmentLibrary : MonoBehaviour
+{
+    public List<Environment> Environments;
+    public SkyBoxController skyBoxController;
+}
+
+[Serializable]
+public class Environment
+{
+    public int WorldRotation;// { get => worldRotation; set => worldRotation = value; }
+    public Texture Background;// { get => background; set => background = value; }
+}
---
+++ 360Spheres/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+using Valve.VR.Extras;
+
+public class PlayerInput : MonoBehaviour
+{
+    public EnvironmentLibrary EnvironmentLibrary;
+
+    [Serializable]
+    public class NewEnvironment : UnityEvent<Environment> { }
+    public NewEnvironment OnNewEnvironment;
+
+    private int currentEnvironmentIndex = 0;
+
+    private void Start()
+    {
+        Select();
+        SteamVR_Actions.default_GrabPinch.AddOnStateDownListener(TriggerPressed, SteamVR_Input_Sources.Any);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void TriggerPressed(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        currentEnvironmentIndex++;
+        UnityEngine.Debug.Log("NextEnv = " + currentEnvironmentIndex);
+
+        if (currentEnvironmentIndex >= EnvironmentLibrary.Environments.Count)
+        {
+            currentEnvironmentIndex = 0;
+        }
+
+        Select();
+    }
+
+    private void Select()
+    {
+        OnNewEnvironment.Invoke(EnvironmentLibrary.Environments[currentEnvironmentIndex]);
+    }
+}
