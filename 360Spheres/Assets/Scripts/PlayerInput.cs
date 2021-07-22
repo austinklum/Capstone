34874e7c731b38893721c7009f8445d9e3be93df
Austin Klum <klumaustin@gmail.com>
2021-07-22 01:46:46 -0500

    Finally have a working example of hitboxes working

--- 360Spheres/Assets/Scripts/PlayerInput.cs
+++ 360Spheres/Assets/Scripts/PlayerInput.cs
@@ -63,7 +63,7 @@ public class PlayerInput : MonoBehaviour
     {
         RaycastHit[] hits;
         hits = Physics.RaycastAll(transform.position, transform.forward, 100.0F);
-
+        Debug.Log("Hits length: " + hits.Length);
         for (int i = 0; i < hits.Length; i++)
         {
             RaycastHit hit = hits[i];
@@ -76,20 +76,28 @@ public class PlayerInput : MonoBehaviour
                 currentObject = hit.collider.gameObject;
 
                 Environment env = EnvironmentLibrary.Environments[currentEnvironmentIndex];
-                
-                Button btnPressed = currentObject.GetComponent<Button>();
-                string btnPressedText = btnPressed.GetComponentInChildren<Text>().text;
-                if (btnPressedText.Contains("2"))
+
+                AnswerButton btnPressed = currentObject.GetComponentInChildren<AnswerButton>();
+                if (IsCorrect(btnPressed.AnswerId))
                 {
                     UnityEngine.Debug.Log("Correct Answer");
-                    Button btnAnswer = currentObject.GetComponent<Button>();
-                    btnAnswer.GetComponentInChildren<Text>().text = "Correct!!";
+                    currentObject.GetComponentInChildren<Text>().text = "Correct!!";
                     getNextQuestion();
                 }
             }
         }
     }
+    private bool IsCorrect(int answerId)
+    {
+        Answer answer = currentQuestion.Answers.FirstOrDefault(ans => ans.AnswerId == answerId);
 
+        if (answer != null)
+        {
+            return answer.IsCorrect;
+        }
+
+        return false;
+    }
     //private void TriggerPressed(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     //{
     //    currentEnvironmentIndex++;
@@ -134,23 +142,15 @@ public class PlayerInput : MonoBehaviour
 
     private void updateBtnAnswers(List<Answer> answers)
     {
-        GameObject pnlAnswers = GameObject.FindGameObjectWithTag("pnlAnswers");
-        Transform btnAnswer1 = pnlAnswers.transform.Find("btnAnswer1");
-        Transform btnAnswer2 = pnlAnswers.transform.Find("btnAnswer2");
-        Transform btnAnswer3 = pnlAnswers.transform.Find("btnAnswer3");
-        Transform btnAnswer4 = pnlAnswers.transform.Find("btnAnswer4");
-
-        btnAnswer1.GetComponentInChildren<Text>().text = answers.First().Content;
-        answers.Remove(answers.First());
-
-        btnAnswer2.GetComponentInChildren<Text>().text = answers.First().Content;
-        answers.Remove(answers.First());
-
-        btnAnswer3.GetComponentInChildren<Text>().text = answers.First().Content;
-        answers.Remove(answers.First());
-
-        btnAnswer4.GetComponentInChildren<Text>().text = answers.First().Content;
-        answers.Remove(answers.First());
+        var tempAnswers = answers.ToList();
+        int count = tempAnswers.Count;
+        for (int i = 1; i <= count; i++)
+        {
+            GameObject btn = GameObject.Find("btnAnswer" + i);
+            btn.GetComponentInChildren<AnswerButton>().AnswerId = tempAnswers.First().AnswerId;
+            btn.GetComponentInChildren<Text>().text = tempAnswers.First().Content;
+            tempAnswers.Remove(tempAnswers.First());
+        }
 
     }
 
