11be7f9a13be2ed545af818744a54c572a39ec69
Austin Klum <klumaustin@gmail.com>
2021-07-22 00:48:00 -0500

    Added AnswerButtons and AnswerButtonsEditor. switched buttons to AnswerButtons

---
+++ 360Spheres/Assets/Scripts/Editor/AnswerButtonEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine;
+
+
+[CustomEditor(typeof(AnswerButton))]
+public class AnswerButtonEditor : ButtonEditor
+{
+    public override void OnInspectorGUI()
+    {
+        AnswerButton targetMyButton = (AnswerButton)target;
+
+        targetMyButton.AnswerId = EditorGUILayout.IntField("AnswerId:", targetMyButton.AnswerId);
+        base.OnInspectorGUI();
+    }
+}
