21e24793a80b3b49660f71850836bcf386b3fa70
Austin Klum <klumaustin@gmail.com>
2021-09-25 16:20:40 -0500

    Pointer Buttons trigger down states

---
+++ PointerProject/Assets/Scripts/VRInputModule.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+public class VRInputModule : BaseInputModule
+{
+    public Camera m_Camera;
+    public SteamVR_Input_Sources m_TargetSource;
+    public SteamVR_Action_Boolean m_ClickAction;
+
+    private GameObject m_CurrentObject = null;
+    private PointerEventData m_Data = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        m_Data = new PointerEventData(eventSystem);
+    }
+
+    // Start is called before the first frame update
+    public override void Process()
+    {
+        m_Data.Reset();
+        m_Data.position = new Vector2(m_Camera.pixelWidth / 2, m_Camera.pixelHeight / 2);
+
+        eventSystem.RaycastAll(m_Data, m_RaycastResultCache);
+        m_Data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        m_CurrentObject = m_Data.pointerCurrentRaycast.gameObject;
+
+        m_RaycastResultCache.Clear();
+
+        HandlePointerExitAndEnter(m_Data, m_CurrentObject);
+
+        if (m_ClickAction.GetStateDown(m_TargetSource))
+        {
+            ProcessPress(m_Data);
+        }
+
+        if (m_ClickAction.GetStateUp(m_TargetSource))
+        {
+            ProcessRelease(m_Data);
+        }
+
+    }
+
+    // Update is called once per frame
+    public PointerEventData GetData()
+    {
+        return m_Data;
+    }
+
+    private void ProcessPress(PointerEventData data)
+    {
+        // Set Raycast
+        data.pointerPressRaycast = data.pointerCurrentRaycast;
+
+        // Check for Object Hit, get the down handler, call
+        GameObject newPointerPress = ExecuteEvents.ExecuteHierarchy(m_CurrentObject, data, ExecuteEvents.pointerDownHandler);
+
+        // If no down handler, try and get click handler
+        if (newPointerPress == null)
+        {
+            newPointerPress = ExecuteEvents.GetEventHandler<IPointerClickHandler>(m_CurrentObject);
+        }
+
+        // Set Data
+        data.pressPosition = data.position;
+        data.pointerPress = newPointerPress;
+        data.rawPointerPress = m_CurrentObject;
+    }
+
+    private void ProcessRelease(PointerEventData data)
+    {
+        // Execute pointer up
+        ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerUpHandler);
+
+        // Check for Click Handler
+        GameObject pointerUpHandler = ExecuteEvents.GetEventHandler<IPointerClickHandler>(m_CurrentObject);
+
+        // Check if actual
+        if (data.pointerPress == pointerUpHandler)
+        {
+            ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerClickHandler);
+        }
+
+        // Clear selected GameObject
+        eventSystem.SetSelectedGameObject(null);
+
+        // Reset Data
+        data.pressPosition = Vector2.zero;
+        data.pointerPress = null;
+        data.rawPointerPress = null;
+
+    }
+}
